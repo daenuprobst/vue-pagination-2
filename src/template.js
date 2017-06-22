519125f1a8d2ec1839f6370e9e14a09ea69085f5
Daniel Probst <daenuprobst@gmail.com>
2017-06-22 16:33:29 +0200

    Page indicators updated

--- src/template.js
+++ src/template.js
@@ -5,37 +5,36 @@ module.exports = function() {
 
   this.pages.map(function(page) {
       items.push(
-        <li class={"VuePagination__pagination-item page-item "+this.activeClass(page)}>
-        <a class="page-link" href="javascript:void(0);"
-        on-click={this.setPage.bind(this, page)}>{page}</a>
+        <li class={"VuePagination__pagination-item "+this.activeClass(page)}>
+        <a class="pagination-link" href="javascript:void(0);" on-click={this.setPage.bind(this, page)}>{page}</a>
         </li>
-        )
+      )
   }.bind(this));
 
   return <nav class="pagination VuePagination">
   <ul v-show={this.totalPages>1}
   class="pagination-list VuePagination__pagination">
 
-  <li class={"VuePagination__pagination-item page-item VuePagination__pagination-item-prev-chunk "+this.allowedChunkClass(-1)}>
-  <a class="page-link" href="javascript:void(0);"
+  <li class={"VuePagination__pagination-item VuePagination__pagination-item-prev-chunk "+this.allowedChunkClass(-1)}>
+  <a class="pagination-link" href="javascript:void(0);"
   on-click={this.setChunk.bind(this,-1)}>&lt;&lt;</a>
   </li>
 
 
-  <li class={"VuePagination__pagination-item page-item VuePagination__pagination-item-prev-page "+this.allowedPageClass(this.page-1)}>
-  <a class="page-link" href="javascript:void(0);"
+  <li class={"VuePagination__pagination-item VuePagination__pagination-item-prev-page "+this.allowedPageClass(this.page-1)}>
+  <a class="pagination-link" href="javascript:void(0);"
   on-click={this.prev.bind(this)}>&lt;</a>
   </li>
 
   {items}
 
-  <li class={"VuePagination__pagination-item page-item VuePagination__pagination-item-next-page "+this.allowedPageClass(this.page+1)}>
-  <a class="page-link" href="javascript:void(0);"
+  <li class={"VuePagination__pagination-item VuePagination__pagination-item-next-page "+this.allowedPageClass(this.page+1)}>
+  <a class="pagination-link" href="javascript:void(0);"
   on-click={this.next.bind(this)}>&gt;</a>
   </li>
 
-  <li class={"VuePagination__pagination-item page-item VuePagination__pagination-item-next-chunk "+this.allowedChunkClass(1)}>
-  <a class="page-link" href="javascript:void(0);"
+  <li class={"VuePagination__pagination-item VuePagination__pagination-item-next-chunk "+this.allowedChunkClass(1)}>
+  <a class="pagination-link" href="javascript:void(0);"
   on-click={this.setChunk.bind(this,1)}>&gt;&gt;</a>
   </li>
   </ul>
